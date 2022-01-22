128ca8a2e2754b820271144688f8cd5ee62946d5
KanchanaSW <sk.personalusername@gmail.com>
2022-01-22 14:47:05 +0530

    Config Package changed loca

--- src/main/java/com/system/Integration/Config/ExConfig.java
+++ src/main/java/com/system/Integration/Config/ExConfig.java
@@ -1,4 +1,4 @@
-package com.system.Config;
+package com.system.Integration.Config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
--- src/main/java/com/system/Integration/Config/MainConfig.java
+++ src/main/java/com/system/Integration/Config/MainConfig.java
@@ -1,4 +1,4 @@
-package com.system.Config;
+package com.system.Integration.Config;
 
 import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Qualifier;
